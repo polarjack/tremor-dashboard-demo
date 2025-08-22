e104cbf48ede5e744fad3ad23d76c4266a5e12dd
Jack Yu <luckyjack85@gmail.com>
2025-08-22 14:16:18 +0800

    chore: update for cloudflare deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+
+if (process.env.NODE_ENV === 'development') {
+  await setupDevPlatform();
+}
 
 const nextConfig = {
+  experimental: {
+    runtime: 'edge',
+  },
   redirects: async () => {
     return [
       {
